486af98664c17909ea0b4b2dad006c69bcdbf341
Pyenb <pyenb@noone.com>
2024-03-14 22:50:59 +0100

    add auto Inno setup build & archiving

--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
 pyinstaller --onefile --noconsole -n "Keychron mice updater" -i images/logo.ico --paths=.venv\Lib\site-packages --upx-dir=upx/ updater.py
+
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" innosetup/setup.iss
+
+powershell.exe -nologo -noprofile -command "& { Compress-Archive -Path 'innosetup\Keychron mice updater setup.exe' -DestinationPath 'innosetup\Keychron_mice_updater_setup.zip' }"
