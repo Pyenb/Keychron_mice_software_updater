26903d03fce012712cfe9fb1199833b2fec39133
Ben <contact.anonyvius@gmail.com>
2024-04-16 14:15:35 +0200

    stop if build fails

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-pyinstaller --onefile --noconsole -n "Keychron mice updater" -i images/logo.ico --paths=.venv\Lib\site-packages --upx-dir=upx/ updater.py
+pyinstaller --onefile --noconsole -n "Keychron mice updater" -i images/logo.ico --paths=.venv\Lib\site-packages --upx-dir=upx/ updater.py || exit /b
 
-"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" innosetup/setup.iss
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" innosetup/setup.iss || exit /b
 
-powershell.exe -nologo -noprofile -command "& { Compress-Archive -Path 'innosetup\Keychron mice updater setup.exe' -DestinationPath 'innosetup\Keychron_mice_updater_setup.zip' }"
+powershell.exe -nologo -noprofile -command "& { Compress-Archive -Path 'innosetup\Keychron mice updater setup.exe' -DestinationPath 'innosetup\Keychron_mice_updater_setup.zip' }" || exit /b
